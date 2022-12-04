53f8281db7a3dae2f6d7b47d2237848909fb2472
Sion Lloyd <sionlloyd101@hotmail.com>
2022-12-04 23:58:01 +0000

    Parse Day4 better

--- Day4/Solution.cs
+++ Day4/Solution.cs
@@ -7,8 +7,13 @@
         private class Pairs
         {
             public string Line { get; set; } = string.Empty;
+            public int Number1 { get; set; }
+            public int Number2 { get; set; }
+            public int Number3 { get; set; }
+            public int Number4 { get; set; }
             public bool FullRange { get; set; } = false;
             public bool PartRange { get; set; } = false;
+            public string Output => $"{Line} - {FullRange} - {PartRange}";
         }
 
         public void Run()
@@ -18,12 +23,21 @@
 
             foreach (var item in list)
             {
+                var array = item.Line.Split(',');
+                var elv1 = array[0].Split('-');
+                var elv2 = array[1].Split('-');
+
+                item.Number1 = int.Parse(elv1[0]);
+                item.Number2 = int.Parse(elv1[1]);
+                item.Number3 = int.Parse(elv2[0]);
+                item.Number4 = int.Parse(elv2[1]);
+
                 item.FullRange = CheckFullRanges(item);
                 item.PartRange = CheckPartRanges(item);
             }
 
             var builder = new OutputBuilder();
-            builder.AppendLines(list.Select(item => $"{item.Line} - {item.FullRange} - {item.PartRange}"));
+            builder.AppendLines(list.Select(item => item.Output));
             builder.AppendLine($"Full range Total: {list.Count(x => x.FullRange)}");
             builder.Append($"Part Range Total: {list.Count(x => x.PartRange)}");
             File.WriteAllText("Day4\\Output.txt", builder.ToString());
@@ -31,26 +45,17 @@
 
         private bool CheckPartRanges(Pairs pairs)
         {
-            var array = pairs.Line.Split(',');
-            var elv1 = array[0].Split('-');
-            var elv2 = array[1].Split('-');
-
-            var num1 = int.Parse(elv1[0]);
-            var num2 = int.Parse(elv1[1]);
-            var num3 = int.Parse(elv2[0]);
-            var num4 = int.Parse(elv2[1]);
-
-            if (num1 >= num3 && num1 <= num4)
+            if (pairs.Number1 >= pairs.Number3 && pairs.Number1 <= pairs.Number4)
             {
                 return true;
             }
 
-            if (num2 >= num3 && num2 <= num4)
+            if (pairs.Number2 >= pairs.Number3 && pairs.Number2 <= pairs.Number4)
             {
                 return true;
             }
 
-            if (num2 >= num4 && num1 <= num4)
+            if (pairs.Number2 >= pairs.Number4 && pairs.Number1 <= pairs.Number4)
             {
                 return true;
             }
@@ -60,21 +65,12 @@
 
         private bool CheckFullRanges(Pairs pairs)
         {
-            var array = pairs.Line.Split(',');
-            var elv1 = array[0].Split('-');
-            var elv2 = array[1].Split('-');
-
-            var num1 = int.Parse(elv1[0]);
-            var num2 = int.Parse(elv1[1]);
-            var num3 = int.Parse(elv2[0]);
-            var num4 = int.Parse(elv2[1]);
-
-            if (num1 <= num3 && num2 >= num4)
+            if (pairs.Number1 <= pairs.Number3 && pairs.Number2 >= pairs.Number4)
             {
                 return true;
             }
 
-            if (num3 <= num1 && num4 >= num2)
+            if (pairs.Number3 <= pairs.Number1 && pairs.Number4 >= pairs.Number2)
             {
                 return true;
             }
