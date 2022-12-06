ab266cdbb43c840112606bc6100ae69d7f395906
Sion Lloyd <sion.lloyd@thinkproject.com>
2022-12-06 12:48:17 +0000

    Implement day 6

---
+++ Day6/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Day6
+{
+    public class Solution : ISolution
+    {
+        private const int DISTINCT = 14;
+
+        public void Run()
+        {
+            var data = File.ReadAllText("Day6\\Input.txt");
+
+            var result = 0;
+            string mark = data.Substring(0, DISTINCT);
+
+            for (int i = DISTINCT; i < data.Length; i++)
+            {
+                var letter = data[i];
+                mark = mark.Substring(1) + letter;
+                if (IsUnique(mark))
+                {
+                    result = i + 1;
+                    break;
+                }
+            }
+
+            File.WriteAllText("Day6\\Output.txt", $"Result : {result}");
+        }
+
+        private bool IsUnique(String str)
+        {
+
+            for (int i = 0; i < str.Length; i++)
+                for (int j = i + 1; j < str.Length; j++)
+                    if (str[i] == str[j])
+                        return false;
+
+            return true;
+        }
+    }
+}
